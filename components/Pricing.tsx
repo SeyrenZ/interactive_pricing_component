630388615a2bfc810ce818938aa868a099c5ec80
Kaiser <kaiserzulkarnain@gmail.com>
2024-01-30 20:24:56 +0700

    UI change

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -50,24 +50,25 @@ const Pricing = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-y-32">
+    <div className="flex flex-col items-center gap-y-32 sm:gap-y-20">
       {/* Header */}
       <div className="flex flex-col items-center gap-y-3">
-        <div className="text-4xl font-extrabold text-[#293356]">
+        <div className="text-4xl sm:text-2xl font-extrabold text-[#293356]">
           Simple, traffic-based pricing
         </div>
-        <div className="text-lg text-[#858fad]">
-          Sign-up for our 30-day trial. No credit card required
+        <div className="text-lg sm:text-sm sm:text-center text-[#858fad]">
+          Sign-up for our 30-day trial.
+          <br className="lg-hidden" /> No credit card required
         </div>
       </div>
       {/* Pricing Component */}
-      <div className="w-[650px] h-auto p-12 pt-14 rounded-lg bg-white shadow-xl flex flex-col gap-y-[50px]">
+      <div className="w-[650px] sm:w-[350px] h-auto p-12 sm:p-8 pt-14 rounded-lg bg-white shadow-xl flex flex-col gap-y-[50px] sm:gap-y-[40px]">
         {/* Price Value */}
-        <div className="flex justify-between items-center">
-          <div className="text-lg font-bold  text-[#858fad] tracking-widest">
+        <div className="flex justify-between items-center sm:justify-center">
+          <div className="text-lg sm:text-sm font-bold  text-[#858fad] tracking-widest">
             {DATA[priceValue].pageView} PAGEVIEWS
           </div>
-          <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-2 sm:hidden">
             <span className="text-5xl font-bold text-[#293356]">
               $
               {!isChecked
@@ -91,44 +92,59 @@ const Pricing = () => {
             id="mySlider"
           />
         </div>
+        <div className="flex items-center justify-center gap-x-2 md:hidden lg:hidden">
+          <span className="text-4xl font-bold text-[#293356]">
+            $
+            {!isChecked
+              ? DATA[priceValue].monthlyPrice
+              : DATA[priceValue].yearlyPrice}
+          </span>
+          <span className="text-lg text-[#858fad] font-bold">
+            {!isChecked ? "/ month" : "/ year"}
+          </span>
+        </div>
         {/* yearly billing */}
-        <div className="flex justify-end pr-8 items-center gap-x-4">
-          <div className="text-md text-[#858fad] font-bold">
+        <div className="flex justify-end pr-8 sm:pr-0 items-center gap-x-4 sm:gap-x-[2]">
+          <div className="text-md sm:text-[11px] text-[#858fad] font-bold">
             Monthly Billing
           </div>
           <label className="switch">
             <input type="checkbox" onChange={handleCheckboxChange} />
             <span className="slider round"></span>
           </label>
-          <div className="text-md text-[#858fad] font-bold">Yearly billing</div>
-          <div className="p-0.5 px-2 rounded-xl text-sm text-[#ff8c66] bg-[#ff8c66] bg-opacity-20">
-            25% discount
+          <div className="flex items-center gap-x-1">
+            <div className="text-md sm:text-[11px] text-[#858fad] font-bold">
+              Yearly billing
+            </div>
+            <div className="p-0.5 px-2 sm:px-1 rounded-xl text-sm sm:text-[10px] text-[#ff8c66] bg-[#ff8c66] bg-opacity-20">
+              -25% <span className="sm:hidden">discount</span>
+            </div>
           </div>
         </div>
         <hr className="w-full" />
         {/* footer */}
-        <div className="flex justify-between items-center">
+        <div className="flex sm:flex-col sm:gap-y-10 justify-between items-center">
           <div className="flex flex-col gap-y-2">
-            <div className="flex items-center gap-x-4 ">
-              <CheckIcon className="w-3" />
-              <div className="text-md text-[#858fad] font-medium">
+            <div className="flex items-center gap-x-4 sm:gap-x-2 ">
+              <CheckIcon className="w-3 sm:w-2" />
+              <div className="text-md sm:text-xs text-[#858fad] font-medium">
                 Unlimited websites
               </div>
             </div>
-            <div className="flex items-center gap-x-4">
-              <CheckIcon className="w-3" />
-              <div className="text-md text-[#858fad] font-medium">
+            <div className="flex items-center gap-x-4 sm:gap-x-2">
+              <CheckIcon className="w-3 sm:w-2" />
+              <div className="text-md sm:text-xs text-[#858fad] font-medium">
                 100% data owndership
               </div>
             </div>
-            <div className="flex items-center gap-x-4">
-              <CheckIcon className="w-3" />
-              <div className="text-md text-[#858fad] font-medium">
+            <div className="flex items-center gap-x-4 sm:gap-x-2">
+              <CheckIcon className="w-3 sm:w-2" />
+              <div className="text-md sm:text-xs text-[#858fad] font-medium">
                 Email reports
               </div>
             </div>
           </div>
-          <button className="w-56 h-14 p-5 rounded-[50px] bg-[#293356] hover:bg-[#1b2139] text-white text-md font-medium flex items-center justify-center">
+          <button className="w-56 h-14 sm:w-48 sm:h-10 p-5 rounded-[50px] bg-[#293356] hover:bg-[#1b2139] text-white text-md sm:text-sm font-medium flex items-center justify-center">
             Start my Trial
           </button>
         </div>
